537924bd86d8c88a1808482334bfc795f60a1ff8
mehnoor888 <mehnoorsayed@gmail.com>
2023-01-18 13:44:39 +0400

    Added content to host pages and corrected responsiveness in some other pages.

---
+++ test.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    var $menu = $("#menu"),
+    $menulink = $(".menu-link");
+    
+    $menulink.click(function() {
+      $menulink.toggleClass("open");
+      $menu.toggleClass("open");
+      return false;
+    });
+  });
+  
